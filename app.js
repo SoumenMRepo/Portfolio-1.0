325850270ea8c60c9c54e9c5b31ccc08f6f2f137
Soumen Mondal <soumenm129@gmail.com>
2020-04-20 01:09:48 +0530

    commit on master

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res) {
-	res.sendFile('/index.html');
+app.get("/", function (req, res) {
+  res.sendFile("/index.html");
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP, function(err) {
-	console.log('sever soumen started.');
+app.listen(process.env.PORT || 3000, process.env.IP, function (err) {
+  console.log("sever soumen started.");
 });
